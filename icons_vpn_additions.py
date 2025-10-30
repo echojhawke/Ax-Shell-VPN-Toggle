d34269bc0269820697dcb85abf5a0867b3bb7fa1
Echo Hawke <echojhawke@gmail.com>
2025-10-29 23:39:08 -0600

    Update icons_vpn_additions.py

--- icons_vpn_additions.py
+++ icons_vpn_additions.py
@@ -3,17 +3,11 @@
 
 # VPN Icons using Google Material Icons
 # Choose whatever icons you want from Google font icons
-# Main VPN button icon (e593)
-vpn_off: str = "&#xe593;"
+# VPN Icons
+vpn_off: str = "&#xed58;"
+vpn_on: str = "&#xf763;"
+vpn_connected: str = "&#xf761;"
+vpn_disconnected: str = "&#xecf8;"
+refresh: str = "&#xf9cd;"
 
-# Connected VPN icon (f686) 
-vpn_on: str = "&#xf686;"
-vpn_connected: str = "&#xf686;"
-
-# Disconnected VPN icon (e9d4)
-vpn_disconnected: str = "&#xe9d4;"
-
-# Refresh icon
-refresh: str = "&#xe5d5;"
-
-# Something something open source license google yada yada look it up
+# Something something open source license Tabler yada yada look it up
